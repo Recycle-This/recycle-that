a66c0f89b4c1fcf1410e3ec92543fc3595cac0fb
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-02-19 11:17:18 -0800

    created template for ProfileScreen

---
+++ screens/ProfileScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { SafeAreaView, Text } from 'react-native';
+
+const ProfileScreen = ({ navigation }) => {
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>This is the ProfileScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default ProfileScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});
