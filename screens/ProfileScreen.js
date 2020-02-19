cceaebd58e96aa0ade881e9add4b9501b2956fca
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-02-19 11:22:00 -0800

    add StyleSheet import for ProfileScreen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
 
 const ProfileScreen = ({ navigation }) => {
 
