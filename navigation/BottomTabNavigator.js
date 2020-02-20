3fcfab695251eae84b63625137f33ae09821a142
Sarah's Mac <sarahsmac@Apples-Mac-4.local>
2020-02-20 10:16:24 -0800

    Final UI for Login, Sign up, and header

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import SearchStackScreen from './SearchStack';
 import ProfileScreenStack from './ProfileStack';
 import TabBarIcon from '../components/TabBarIcon';
+import LoginScreen from '../screens/LoginScreen';
 
 const BottomTab = createBottomTabNavigator();
 const INITIAL_ROUTE_NAME = 'Search';
@@ -14,7 +15,7 @@ export default function BottomTabNavigator({ navigation, route }) {
     <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
       <BottomTab.Screen
         name="Search"
-        component={SearchStackScreen}
+        component={LoginScreen}
         options={{
           title: 'Search',
           tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,38 +1,51 @@
 import React from 'react';
-import { Container, Header, Item, Input, Icon, Button, Text, Content, List, ListItem, Left, Right } from 'native-base';
+import {
+  Container,
+  Header,
+  Item,
+  Input,
+  Icon,
+  Button,
+  Text,
+  Content,
+  List,
+  ListItem,
+  Left,
+  Right
+} from 'native-base';
 import { SafeAreaView, TouchableHighlight, View, StyleSheet } from 'react-native';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 
 const SearchScreen = ({ navigation }) => {
-  const top = ['CFL Lightbulbs', 'Paper','Aluminum Cans','Cardboard', 'Newspapers', 'Plastic Bottles', 'Glass Containers', 'Glossy Magazines','Oil-Based Paints','Christmas Trees'];
-  let topList = top.map((item,i) => {
-    return(
-      <ListItem key={i} noIndent style={{ backgroundColor: "white" }}>
+  const top = ['CFL Lightbulbs', 'Paper', 'Aluminum Cans', 'Cardboard', 'Newspapers', 'Plastic Bottles', 'Glass Containers', 'Glossy Magazines', 'Oil-Based Paints', 'Christmas Trees'];
+  let topList = top.map((item, i) => {
+    return (
+      <ListItem key={i} noIndent style={{ backgroundColor: '#FDFAEC' }}>
         <Left>
           <Text>{item}</Text>
         </Left>
         <Right>
-          <Icon name="arrow-forward"/>
+          <Icon name="arrow-forward" />
         </Right>
       </ListItem>
-    );    
+    );
   });
   return (
     <Container>
-      <Header searchBar rounded style={{backgroundColor: "green"}}>
+      <Header searchBar rounded style={{ backgroundColor: "green" }}>
         <Item>
           <Icon name="ios-search" />
           <Input placeholder="Search" />
           <Icon name="ios-people" />
         </Item>
         <Button transparent>
-          <Text style={{color:'white'}}>Go!</Text>
+          <Text style={{ color: 'white' }}>Go!</Text>
         </Button>
       </Header>
       <Content >
-        <Text style={{textAlign: "center", color:"green", fontWeight:"bold", marginTop: 10, marginBottom:10}}>Top 10 Recyclables </Text>
-        <List style={{marginRight: 5, marginLeft:5, borderWidth:1, borderColor:"#D3D3D3"  }}>
+        <Text style={{ textAlign: "center", color: "green", fontWeight: "bold", marginTop: 10, marginBottom: 10 }}> Top 10 Recyclables </Text>
+        <List style={{ marginRight: 5, marginLeft: 5, borderWidth: 1, borderColor: "#D3D3D3", }}>
           {topList}
         </List>
       </Content>
