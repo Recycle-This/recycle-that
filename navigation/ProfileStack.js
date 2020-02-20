ad9414c86abfedf6bf20406ed78670d811fd3e5d
Sarah's Mac <sarahsmac@Apples-Mac-4.local>
2020-02-19 22:05:01 -0800

    Header

--- navigation/ProfileStack.js
+++ navigation/ProfileStack.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { Image } from 'react-native';
 import ProfileScreen from '../screens/ProfileScreen';
 
+function LogoTitle() {
+  return (
+    <Image style={{ width: 160, height: 60 }} source={require('../assets/images/logo.png')} />
+  );
+}
 const ProfileStack = createStackNavigator();
 
 const ProfileScreenStack = ({ navigation, route }) => {
 
   return (
     <ProfileStack.Navigator initialRouteName={'Profile'} >
-      <ProfileStack.Screen name="Profile" component={ProfileScreen} />
+      <ProfileStack.Screen name="Profile" component={ProfileScreen} options={{ headerTitle: props => <LogoTitle {...props} /> }} />
     </ProfileStack.Navigator>
   );
 
--- navigation/SearchStack.js
+++ navigation/SearchStack.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { Image } from 'react-native';
 import SearchScreen from '../screens/SearchScreen';
 import DetailsScreen from '../screens/DetailsScreen';
 
 const SearchStack = createStackNavigator();
-
+function LogoTitle() {
+  return (
+    <Image style={{ width: 160, height: 60 }} source={require('../assets/images/logo.png')} />
+  );
+}
 const SearchStackScreen = ({ navigation, route }) => {
 
   return (
-    <SearchStack.Navigator initialRouteName={'Search'} headerTitle={'truyeee'}>
-      <SearchStack.Screen name="Search" component={SearchScreen} />
-      <SearchStack.Screen name="Details" component={DetailsScreen} />
+    <SearchStack.Navigator initialRouteName={'Search'} headerTitle={'Recycle'}>
+      <SearchStack.Screen name="Recycle That" component={SearchScreen} options={{ headerTitle: props => <LogoTitle {...props} /> }} />
+
+      <SearchStack.Screen name="Details" component={DetailsScreen} options={{ headerTitle: props => <LogoTitle {...props} /> }} />
     </SearchStack.Navigator>
   );
 };
