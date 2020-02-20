24cd801431d33088c76efb8de174efb5b1e050f0
Chloe Aribo <c.aribo@Chloes-MacBook-Pro.local>
2020-02-20 11:36:49 -0800

    fetches edited

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,8 +4,7 @@ import {
   Text,
   Image,
   TextInput,
-  TouchableOpacity,
-  StyleSheet
+  TouchableOpacity
 } from 'react-native';
 
 
@@ -46,75 +45,24 @@ const LoginScreen = ({ navigation }) => {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.image}>
-        <Image source={require('../assets/images/logo.png')} />
-      </View>
-      <TextInput onChangeText={text => setLoginState({ ...loginState, username: text })} style={styles.input}
+    <View>
+      <Image source={require('../assets/images/logo.png')} />
+
+      <TextInput onChangeText={text => setLoginState({ ...loginState, username: text })} style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
         placeholder="Username" />
-      <TextInput onChangeText={text => setLoginState({ ...loginState, password: text })} style={styles.input}
+      <TextInput onChangeText={text => setLoginState({ ...loginState, password: text })} style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
         placeholder="Password" />
-      <View style={styles.buttonContainer} >
-        <TouchableOpacity onPress={handleLogin}><Text style={styles.buttonText}>Login</Text></TouchableOpacity>
-      </View>
-      <View >
-        <Text>Forgot your login details?</Text><TouchableOpacity onPress={handleLogin} ><Text style={{
-          color: "blue", textAlign: 'center',
-        }}>Get help signing in.</Text></TouchableOpacity>
+      <View>
+        <TouchableOpacity onPress={handleLogin} style={{ borderColor: 'gray', borderWidth: 1 }}><Text>Login</Text></TouchableOpacity>
       </View>
       <View>
-        <Text>Don't have an account?</Text><TouchableOpacity onPress={handleSignupPress} ><Text style={{ color: "blue", textAlign: 'center' }}>Sign Up.</Text></TouchableOpacity>
+        <Text>Forgot your login details?</Text><TouchableOpacity onPress={handleLogin} ><Text>Get help signing in.</Text></TouchableOpacity>
+      </View>
+      <View>
+        <Text>Don't have an account?</Text><TouchableOpacity onPress={handleSignupPress} ><Text>Sign Up.</Text></TouchableOpacity>
       </View>
     </View >
   )
 }
 
 export default React.memo(LoginScreen);
-
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    width: 420,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 80,
-    paddingTop: 95,
-    paddingBottom: 270,
-  },
-  input: {
-    height: 40,
-    backgroundColor: 'rgb(255, 255, 255)',
-    marginBottom: 20,
-    width: 200,
-    paddingHorizontal: 10,
-    color: 'black',
-    borderColor: 'gray',
-    borderWidth: 1
-  },
-  buttonContainer: {
-    backgroundColor: '#23A75B',
-    paddingVertical: 15,
-    borderRadius: 5,
-    marginTop: 10,
-    marginBottom: 40
-  },
-  buttonText: {
-    textAlign: 'center',
-    color: 'white',
-    fontWeight: '700',
-    fontSize: 16,
-    width: 175
-  },
-  addText: {
-    textAlign: 'center',
-    color: 'white',
-    fontWeight: '700',
-    fontSize: 16,
-  },
-  image: {
-    paddingBottom: 25,
-    paddingTop: 40,
-  }
-
-});
