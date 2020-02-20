9fd0c48dd390c832301a7027ed84c906d1392ad1
Sarah's Mac <sarahsmac@Apples-Mac-4.local>
2020-02-19 19:50:58 -0800

    login styling

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -4,7 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import SearchStackScreen from './SearchStack';
 import ProfileScreenStack from './ProfileStack';
 import TabBarIcon from '../components/TabBarIcon';
-
+import SignUpScreen from '../screens/SignUpScreen';
 const BottomTab = createBottomTabNavigator();
 const INITIAL_ROUTE_NAME = 'Search';
 
@@ -14,7 +14,7 @@ export default function BottomTabNavigator({ navigation, route }) {
     <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
       <BottomTab.Screen
         name="Search"
-        component={SearchStackScreen}
+        component={SignUpScreen}
         options={{
           title: 'Search',
           tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, Image, TouchableHighlight, TextInput } from 'react-native';
+import { View, Text, Image, TouchableHighlight, TextInput, StyleSheet } from 'react-native';
 
 const SignUpScreen = (props) => {
   const [signInState, setSignInState] = useState({
@@ -24,19 +24,20 @@ const SignUpScreen = (props) => {
     }
   }
   return (
-    <View>
+    <View style={styles.container}>
+      <Image source={require('../assets/images/logo.png')} />
       <Text>Create UserName</Text>
-      <TextInput onChangeText={text => setSignInState({ ...signInState, username: text })} placeholder="Please be at least 3 characters long"></TextInput>
+      <TextInput style={styles.input} onChangeText={text => setSignInState({ ...signInState, username: text })} placeholder="Please be at least 3 characters long"></TextInput>
       <View>
         <Text>Password</Text>
-        <TextInput onChangeText={text => setSignInState({ ...signInState, password: text })} placeholder="password"></TextInput>
+        <TextInput style={styles.input} onChangeText={text => setSignInState({ ...signInState, password: text })} placeholder="password"></TextInput>
       </View>
       <View>
         <Text>Reenter Password</Text>
-        <TextInput onChangeText={text => setSignInState({ ...signInState, verifyPassword: text })} placeholder="password"></TextInput>
+        <TextInput style={styles.input} onChangeText={text => setSignInState({ ...signInState, verifyPassword: text })} placeholder="password"></TextInput>
       </View>
       <View>
-        <TouchableHighlight onPress={checkUsernameLength}><Text>Sign Up</Text></TouchableHighlight>
+        <TouchableHighlight style={styles.buttonContainer} onPress={checkUsernameLength}><Text style={styles.buttonText} >Sign Up</Text></TouchableHighlight>
       </View>
     </View>
 
@@ -44,3 +45,41 @@ const SignUpScreen = (props) => {
 }
 
 export default React.memo(SignUpScreen);
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    width: 325,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 80,
+    paddingTop: 35,
+  },
+  input: {
+    height: 40,
+    backgroundColor: 'rgb(255, 255, 255)',
+    marginBottom: 20,
+    width: 200,
+    paddingHorizontal: 10,
+    color: 'black',
+  },
+  buttonContainer: {
+    backgroundColor: '#1589FF',
+    paddingVertical: 15,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+    width: 175
+  },
+  addText: {
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+});
