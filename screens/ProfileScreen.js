cea1eca03944f3f812cdb9bfd804210cba64e3db
Taylor Rodrigues <taylor.d.rod33@gmail.com>
2020-02-19 16:37:55 -0800

    addeed profilescreen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
-import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import { SafeAreaView, Text, TouchableHighlight, View, StyleSheet } from 'react-native';
 
-const ProfileScreen = ({ navigation }) => {
+const ProfileScreen = ({ navigation, route }) => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>This is the Profile Screen</Text>
+      <Text>This is the Profile Scrfeen</Text>
+      <View>
+        <TouchableHighlight
+          style={styles.navBtn}
+          onPress={() => navigation.navigate('Details')}
+        >
+          <Text>Navigate to Details</Text>
+        </TouchableHighlight>
+      </View>
     </SafeAreaView>
   );
 };
@@ -16,5 +24,11 @@ export default ProfileScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
+  },
+  navBtn: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'yellow'
   }
 });
